58837bdabfdb92f7e2f6fdc97da3e117838be5d6
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-07 15:01:32 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,4 +5,17 @@ if current_time.to_i % 2 == 0
 else
   puts "Odd!"
 end
-  
+
+#Solution 1:
+# if Time.now.to_i % 2 == 0 
+# puts "Even!"
+# else
+# puts "Odd!"
+#end
+
+#Solution 2:
+# if current_time.even?
+#puts "Even!"
+#else
+#puts "Odd!"
+# end
